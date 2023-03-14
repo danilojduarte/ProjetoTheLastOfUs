bfcc721b13e103ea097c2dfd03039f8b53e05af5
DaniloDuarte <danilojoseduarte@gmail.com>
2023-03-14 01:48:48 -0300

    Projeto finalizado

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
 const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.img');
 
 botoesCarrossel.forEach((botao, indice) => {
     botao.addEventListener('cick', () => {
 
-        console.log(indice);
 
-        const botaoSelecionado = document.querySelector('.selecionado');
-        botaoSelecionado.classList.remove('selecionado');
+        desativarBotaoSelecionado();
 
         botao.classList.add('selecionado');
 
-        const imagemAtiva = document.querySelector('.ativa');
-        imagemAtiva.classList.remover('ativa');
+        botao.classList.add('selecionado');
 
-        
+        esconderImagemAtiva();
 
-    })
-})
+        mostrarImagemDeFundo(indice);
+
+    });
+});
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa');
+    botoesCarrossel[indice].classList.add('selecionado');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
