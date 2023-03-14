cb8dba27c80c03009dd4c44de146cdcd66a6c861
DaniloDuarte <danilojoseduarte@gmail.com>
2023-03-14 01:33:46 -0300

    Finalizando carrossel JavaScript

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 const botoesCarrossel = document.querySelectorAll('.botao');
 
-botoesCarrossel.forEach((botao) => {
+botoesCarrossel.forEach((botao, indice) => {
     botao.addEventListener('cick', () => {
-        const botaoSelecionado= document.querySelector('.selecionado');
+
+        console.log(indice);
+
+        const botaoSelecionado = document.querySelector('.selecionado');
+        botaoSelecionado.classList.remove('selecionado');
+
+        botao.classList.add('selecionado');
+
+        const imagemAtiva = document.querySelector('.ativa');
+        imagemAtiva.classList.remover('ativa');
+
+        
+
     })
 })
