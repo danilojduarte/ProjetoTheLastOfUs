dd26b92134884fe6fe416610cba3b73c76ba5ee2
DaniloDuarte <danilojoseduarte@gmail.com>
2023-03-14 01:14:53 -0300

    Continuando projeto, JavaScript

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ const botoesCarrossel = document.querySelectorAll('.botao');
 
 botoesCarrossel.forEach((botao) => {
     botao.addEventListener('cick', () => {
-        
+        const botaoSelecionado= document.querySelector('.selecionado');
     })
 })
